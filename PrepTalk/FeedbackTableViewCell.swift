853b7f737c4bd7b5355d09dd86cced4fe00c98bc
Jose Patino <jose.patino88@icloud.com>
2020-02-08 12:56:20 -0500

    Cells

---
+++ PrepTalk/FeedbackTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedbackTableViewCell.swift
+//  PrepTalk
+//
+//  Created by Jose Patino on 2/8/20.
+//  Copyright © 2020 Jose Patino. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
